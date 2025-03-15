714c83d1c3502a2af6337988eebbb7a335184a35
Nick <nkuuee@bk.ru>
2025-03-15 18:59:53 +0300

    Класс для регистрации новых пользователей завершен

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,5 +1,7 @@
 package service;
 
+import utils.PasswordUtils;
+
 import java.sql.*;
 
 public class UserService {
@@ -11,21 +13,49 @@ public class UserService {
         return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
     }
 
-    public Boolean equalsUser(String username) throws SQLException {
+    // Проверка, существует ли пользователь в БД
+    public boolean userExists(String username) {
         try (Connection connection = getConnection()) {
             String sql = "SELECT COUNT(*) FROM users WHERE username = ?";
             try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
                 preparedStatement.setString(1, username);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                if (resultSet.next()) {
-                    return resultSet.getInt(1) > 0;
+                ResultSet rs = preparedStatement.executeQuery();
+                if (rs.next()) {
+                    return rs.getInt(1) > 0; // Если хотя бы один пользователь найден
                 }
-            } catch (SQLException e) {
-                e.printStackTrace();
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    // Регистрация нового пользователя
+    public boolean registerUser(String username, String password) {
+        // Проверка, существует ли уже пользователь с таким именем
+        if (userExists(username)) {
+            System.out.println("Пользователь с таким именем уже существует.");
             return false;
         }
 
+        //Хэшируем пароль
+        String hashPassword = PasswordUtils.hashPassword(password);
+
+        //Сохраняем в БД
+        try (Connection connection = getConnection()) {
+            String sql = "INSERT INTO users(username,password) VALUES (?,?)";
+            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setString(1, username);
+                preparedStatement.setString(1, hashPassword);
+                preparedStatement.executeUpdate();
+                return true; //Регистрация прошла успешна
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false; //Ошибка при регистрации
     }
 
 }
+
+
